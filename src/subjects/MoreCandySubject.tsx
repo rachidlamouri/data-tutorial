1e7d9d3e1dc740d27a55572f755fb5bf048cc910
Rachid Lamouri <rachidlamouri@gmail.com>
2025-02-15 15:17:35 -0500

    Remove unused component

--- src/subjects/MoreCandySubject.tsx
+++ src/subjects/MoreCandySubject.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@mui/material';
+import { Button } from '@mui/material';
 import { BigPicture } from '../layout/BigPicture';
 import { Subject } from '../layout/subject/Subject';
 import { useLearnableContext } from '../learnable-provider/LearnableProvider';
